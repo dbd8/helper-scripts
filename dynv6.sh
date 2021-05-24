3f70a86826349b9dce618fb342e92dc8a9dd2f72
dbd8 <daniel-1989@gmx.de>
2021-05-24 21:11:07 +0200

    feat: improved ipv6 address change check via nslookup dns entry

--- dynv6.sh
+++ dynv6.sh
@@ -2,7 +2,11 @@
 hostname=$1
 device=$2
 file=$HOME/.dynv6.addr6
-[ -e $file ] && old=`cat $file`
+#[ -e $file ] && old=`cat $file`
+
+old=$(nslookup db-cloud.dynv6.net | sed -n 's/.*Address: \([0-9a-f:]\+\).*/\1/p')
+
+date=$(date)
 
 if [ -z "$hostname" -o -z "$token" ]; then
   echo "Usage: token=<your-authentication-token> [netmask=64] $0 your-name.dynv6.net [device]"
@@ -35,13 +39,16 @@ fi
 # address with netmask
 current=$address/$netmask
 
-if [ "$old" = "$current" ]; then
-  echo "IPv6 address unchanged"
+if [ "$old" = "$address" ]; then
+  echo "IPv6 address unchanged - $date - $current"
   exit
 fi
 
+echo "IPv6 address changed - $date - $old -> $current"
+
 # send addresses to dynv6
 $bin "https://ipv6.dynv6.com/api/update?hostname=$hostname&ipv6=$current&token=$token"
-
+# new line
+echo ""
 # save current address
 echo $current > $file
